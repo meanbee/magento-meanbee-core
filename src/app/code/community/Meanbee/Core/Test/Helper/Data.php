c9245690b2cb6f204e9bf2be8fa62fe4972b1f7e
Tomas Gerulaitis <tomas.gerulaitis@meanbee.com>
2015-04-03 10:29:33 +0100

    Fix code style

--- src/app/code/community/Meanbee/Core/Test/Helper/Data.php
+++ src/app/code/community/Meanbee/Core/Test/Helper/Data.php
@@ -1,21 +1,26 @@
 <?php
 
-class Meanbee_Core_Test_Helper_data extends EcomDev_PHPUnit_Test_Case {
+class Meanbee_Core_Test_Helper_data extends EcomDev_PHPUnit_Test_Case
+{
+
     /** @var Meanbee_Core_Helper_Data */
     protected $_helper;
 
-    public function setUp() {
+    public function setUp()
+    {
         $this->_helper = new Meanbee_Core_Helper_Data();
     }
 
-    public function tearDown() {
+    public function tearDown()
+    {
         $this->_helper = null;
     }
 
     /**
      * @test
      */
-    public function testFactory() {
+    public function testFactory()
+    {
         $this->assertInstanceOf('Meanbee_Core_Helper_Data', Mage::helper('meanbee_core'));
     }
 }
