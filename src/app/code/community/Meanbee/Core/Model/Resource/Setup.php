0e381b0f42f3520a62bb0102b849edf2db880968
Tomas Gerulaitis <tomas.gerulaitis@meanbee.com>
2015-04-03 14:21:38 +0100

    Add methods to create and update CMS pages

--- src/app/code/community/Meanbee/Core/Model/Resource/Setup.php
+++ src/app/code/community/Meanbee/Core/Model/Resource/Setup.php
@@ -90,19 +90,57 @@ class Meanbee_Core_Model_Resource_Setup extends Mage_Catalog_Model_Resource_Setu
         $layoutUpdateXml = "",
         $stores = array(0)
     ) {
+        $current_store = Mage::app()->getStore();
+        Mage::app()->setCurrentStore(Mage_Core_Model_App::ADMIN_STORE_ID);
+
+        Mage::getModel("cms/page")
+            ->setData(array(
+                "identifier"        => $identifier,
+                "title"             => $title,
+                "content"           => $content,
+                "root_template"     => $rootTemplate,
+                "content_heading"   => $contentHeading,
+                "meta_keywords"     => $metaKeywords,
+                "meta_description"  => $metaDescription,
+                "layout_update_xml" => $layoutUpdateXml,
+                "is_active"         => 1,
+                "stores"            => $stores
+            ))
+            ->save();
+
+        Mage::app()->setCurrentStore($current_store);
         return $this;
     }
 
     /**
      * Update an existing CMS page.
      *
-     * @param string $identifier
-     * @param array  $data A key => value list of new data to assign to the page.
+     * @param string   $identifier
+     * @param array    $data    A key => value list of new data to assign to the page.
+     * @param int|null $storeId Store ID the page is currently assigned to.
      *
      * @return $this
      */
-    public function updateCmsPage($identifier, $data)
+    public function updateCmsPage($identifier, $data, $storeId = null)
     {
+        $current_store = Mage::app()->getStore();
+        Mage::app()->setCurrentStore(Mage_Core_Model_App::ADMIN_STORE_ID);
+
+        $page = Mage::getModel("cms/page");
+        if ($storeId) {
+            $page->getStoreId($storeId);
+        }
+        $page->load($identifier);
+
+        if ($page->getId()) {
+            $page
+                ->addData($data)
+                ->save();
+        } else {
+            Mage::throwException(sprintf("Unable to update CMS page: CMS page '%s' does not exist.", $identifier));
+        }
+
+        Mage::app()->setCurrentStore($current_store);
         return $this;
     }
 
